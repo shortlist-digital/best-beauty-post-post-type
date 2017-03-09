66f51e0ec8119989ca5269bd97d20a3772da4b64
Jon Sherrard <jon@andthats.it>
2017-03-09 12:03:01 +0000

    init

---
+++ best-beauty-post-post-type.php
@@ -0,0 +1,20 @@
+
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: best-beauty-post-post-type
+* Plugin URI: http://github.com/shortlist-digital/best-beauty-post-post-type
+* Description: A beauty post type for Best Beauty
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+class BestBeautyPostPostType
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new BestBeautyPostPostType();
---
+++ readme.md
@@ -0,0 +1,2 @@
+# best-beauty-post-post-type
+A beauty post type for Best Beauty
